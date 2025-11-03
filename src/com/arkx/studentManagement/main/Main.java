e2094f2f3a4eba85041203c8e6cee2e04f8b3746
DEVCHBOM <zaka.ratayl2@gmail.com>
2025-11-03 16:05:56 +0100

    add d'un test
    ajouter un test

--- src/com/arkx/studentManagement/main/Main.java
+++ src/com/arkx/studentManagement/main/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
+        	System.out.println("test1");
             displayMenu();
             int choice = getChoice();
             
