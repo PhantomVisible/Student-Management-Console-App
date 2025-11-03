678594160b592e7c309ef5bf787909bdc8d164e0
ilyassfaquih <ilyassfaquih3@gmail.com>
2025-11-03 16:12:23 +0100

    Update Main.java
    commit

--- src/com/arkx/studentManagement/main/Main.java
+++ src/com/arkx/studentManagement/main/Main.java
@@ -13,7 +13,6 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-//youness bidawi
             displayMenu();
             int choice = getChoice();
             
